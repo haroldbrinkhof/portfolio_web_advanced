98691c1f6d8ba25d663b5f81e4db00947048d1d6
Harold Brinkhof <harold.brinkhof@gmail.com>
2024-06-01 17:39:35 +0200

    initial keep cat functionality
    stores cat data as an array in localStorage
    shows a new cat to keep or reject

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const API_URL = 'https://api.thecatapi.com/v1/images/search'
 const API_KEY = ''
 
+let currentCat;
+
 const headers = new Headers({
   "Content-Type": "application/json",
   "x-api-key": 'live_2JBdfvQsLKE8va2JBKyICewZyTATbv7y4rvmxumUjHZd14v6ZnfbTMdDoz0qqhAc'
@@ -10,6 +12,7 @@ const requestOptions = {
   headers: headers,
   redirect: 'follow'
 };
+
 
 const retrieveCatURL = async () => {
 
@@ -25,7 +28,8 @@ const retrieveCatURL = async () => {
 //	return URL.createObjectURL(catImage);
 	const body = await response.json();
 	console.log(body);
-	return body[0];
+	currentCat = body[0];
+	return currentCat;
 	
 }
 
@@ -40,6 +44,7 @@ const displayCat = (catData) => {
 		
 		window.setTimeout(() => {
 
+			// consulted https://stackoverflow.com/questions/9251837/how-to-remove-all-listeners-in-an-element
 			const newImgEl = mugshotImgEl.cloneNode(true);
 			mugshotImgEl.parentNode.replaceChild(newImgEl, mugshotImgEl);
 			newImgEl.src = catData.url;
@@ -59,7 +64,16 @@ const rejectCat = () => {
 	console.log('rejecting');
 	retrieveCatURL().then(displayCat);
 }
-const keepCat = () => console.log('keeping');
+const keepCat = () => {
+	// add cat to the collection
+	// consulted https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+	const catCollection = localStorage.getItem('catCollection') ? JSON.parse(localStorage.getItem('catCollection')) : [];
+
+	localStorage.setItem('catCollection', JSON.stringify([currentCat, ...catCollection]));
+	console.log('keeping');
+	console.log(JSON.parse(localStorage.getItem('catCollection')));
+	retrieveCatURL().then(displayCat);
+}
  
 // Self executing function to display the initial cat
 // adding event handlers to buttons
